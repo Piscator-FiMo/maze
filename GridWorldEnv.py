a9e28870fc6f811d2d8277d4c9ff71a1d634b7b6
Vincent Genecand <vincent.genecand@gmail.com>
2025-01-18 14:04:27 +0100

    track total reward

--- GridWorldEnv.py
+++ GridWorldEnv.py
@@ -17,6 +17,7 @@ class GridWorldEnv(gym.Env):
         self.clock = None
         self.metadata = {"render_fps": 5}
         self.steps = 0
+        self.total_reward = 0
         self.max_distance = np.linalg.norm(
             np.array([self.labyrinth.columns - 1, self.labyrinth.rows - 1]) - np.array([0, 0]), ord=1
         )
@@ -72,6 +73,7 @@ class GridWorldEnv(gym.Env):
         super().reset(seed=seed)
         self.render_mode = options["render_mode"]
         self.steps = 0
+        self.total_reward = 0
 
         self.labyrinth.regenerate_start_and_end()
         self._agent_location = np.array(self.labyrinth.start.position, dtype=np.int32)
@@ -109,12 +111,14 @@ class GridWorldEnv(gym.Env):
         observation = self._get_obs()
         info = self._get_info()
 
+        self.total_reward += reward
+
         if self.render_mode == "human":
             self._render_frame()
         if truncated:
-            print(f"truncated after {self.steps} steps")
+            print(f"truncated after {self.steps} steps and a total reward of {self.total_reward}")
         if terminated:
-            print(f"reached target after {self.steps} steps")
+            print(f"reached target after {self.steps} steps and a total reward of {self.total_reward}")
 
         return observation, reward, terminated, truncated, info
 
@@ -143,6 +147,7 @@ class GridWorldEnv(gym.Env):
         if self.clock is None and self.render_mode == "human":
             self.clock = pygame.time.Clock()
 
+        pygame.display.set_caption(f'Steps: {self.steps}, Total Reward: {self.total_reward}')
         canvas = pygame.Surface(size)
         # The size of a single grid square in pixels
         canvas.fill((0, 0, 0))
